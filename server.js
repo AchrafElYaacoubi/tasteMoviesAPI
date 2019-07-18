b371aa789d86c8d62961a0d55bb270e45728d5b9
AchrafElYaacoubi <achraf18el@gmail.com>
2019-07-18 12:05:05 +0100

    feat(server) : running the server

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const initilizeDB = require('./app/utils/initilizeDB');
+
+// create express app
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+    initilizeDB();
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Taste Movies application."});
+});
+
+// Require movies routes
+require('./app/routes/movie.routes')(app);
+
+// listen for requests
+app.listen(4000, () => {
+    console.log("Server is listening on port 4000");
+});
